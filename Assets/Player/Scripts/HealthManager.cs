527422ce6ba7388861090b9e820e142c1e88396c
Mark Bennison <mark.bennison@live.co.uk>
2023-02-09 14:49:10 +0000

    Added HealthBar

--- Assets/Player/Scripts/HealthManager.cs
+++ Assets/Player/Scripts/HealthManager.cs
@@ -1,21 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthManager : MonoBehaviour
 {
-    [SerializeField]
-    float hitPoints = 100f;
+	[SerializeField] float maxHitPoints = 100f;
+	float hitPoints;
+
+    public Slider healthSlider;
+
+    void Start()
+    {
+		hitPoints = maxHitPoints;
+	}
 
     void Hit(float rawDamage)
     {
         hitPoints -= rawDamage;
+        SetHealthSlider();
 
-        Debug.Log("OUCH: " + hitPoints.ToString());
+		Debug.Log("OUCH: " + hitPoints.ToString());
 
         if (hitPoints <= 0)
         {
             Debug.Log("TODO: GAME OVER - YOU DIED");
         }
+    }
+
+    void SetHealthSlider()
+    {
+        if (healthSlider != null)
+        {
+            healthSlider.value = NormalisedHitPoint();
+		}
+    }
+
+    float NormalisedHitPoint()
+    {
+		return hitPoints / maxHitPoints;
     }
 }
